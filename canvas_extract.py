24986614a7237ef1f463f5b8ae3868fd7b28d567
Deborah Mundorff <57678987+mundorfd@users.noreply.github.com>
2024-02-27 09:36:01 -0800

    prompting for source and destination directories

--- canvas_extract.py
+++ canvas_extract.py
@@ -6,15 +6,23 @@ def extract_text_from_html(file_path):
         soup = BeautifulSoup(file, 'lxml')
         return soup.get_text()
 
-def search_and_convert(directory):
-    for root, dirs, files in os.walk(directory):
+def search_and_convert(source_directory, destination_directory):
+    for root, dirs, files in os.walk(source_directory):
         for file in files:
             if file.endswith('.html'):
                 file_path = os.path.join(root, file)
                 text = extract_text_from_html(file_path)
-                text_file_path = os.path.splitext(file_path)[0] + '.txt'
+                text_file_name = os.path.splitext(file)[0] + '.txt'
+                text_file_path = os.path.join(destination_directory, text_file_name)
                 with open(text_file_path, 'w', encoding='utf-8') as text_file:
                     text_file.write(text)
 
-# Replace 'your_directory_path' with the path to the directory containing HTML files
-search_and_convert('/Users/mundorfd/Dropbox/Projects/qmreviewgeog/qm-review-geog-380-export')
+# Prompt for source and destination directories
+source_directory = input("Enter the source directory path containing HTML files: ")
+destination_directory = input("Enter the destination directory path for text files: ")
+
+# Create the destination directory if it doesn't exist
+if not os.path.exists(destination_directory):
+    os.makedirs(destination_directory)
+
+search_and_convert(source_directory, destination_directory)
